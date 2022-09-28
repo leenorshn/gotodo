ac94163e8d510090d8ea6efcecf1d5c115b28729
leenorshn <katembovictor@gmail.com>
2022-09-28 12:21:02 +0200

    first go on vercel

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/leenorshn/gotodo/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
